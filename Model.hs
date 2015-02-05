9efeac7f5aac1c182e382cb31f84aebd14cd5312
Kevin Meredith <kevin.m.meredith@gmail.com>
2015-02-04 23:28:06 -0500

    Not yet working ADT for JSON.

---
+++ Model.hs
@@ -0,0 +1,15 @@
+{-# OPTIONS_GHC -Wall #-}
+module Model where
+
+type Key = String
+
+data Json a b = JObj Key JValue 
+               | Arr [JValue] 
+               deriving Show
+
+data JValue = I Int 
+              | F Float 
+              | S String
+              | B Bool
+              | J [Json Key JValue]
+               deriving Show
