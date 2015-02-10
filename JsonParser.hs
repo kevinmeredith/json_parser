d3e57b11a80fd7fe76a331f125ade41e2d68f8f5
Kevin Meredith <kevin.m.meredith@gmail.com>
2015-02-09 22:45:58 -0500

    Began to implement 'parsePositiveDecimal', but ran into type error.

--- JsonParser.hs
+++ JsonParser.hs
@@ -7,15 +7,8 @@ import AParser
 import Control.Applicative
 import Data.Char
 
-parseQuote :: Parser Char
-parseQuote = satisfy (== '"')
-
-parseNotQuote :: Parser Char
-parseNotQuote = satisfy (/= '"')
-
--- TODO: handle escaped \" - " foo \" bar BIZ "
---parseStringJValue :: Parser JValue
---parseStringJValue = (\x -> S x) <$> (parseQuote *> spaces *> (zeroOrMore parseNotQuote) <* parseQuote)
+parseStringJValue :: Parser JValue
+parseStringJValue = (\x -> S x) <$> zeroOrMore (satisfy (const True))
 
 --parseNumberJValue :: Parser JValue
 --parseNumberJValue = alt parseUnsignedNumber
@@ -26,11 +19,27 @@ parsePositiveInteger = fmap (\x -> N (fromIntegral x)) parseInteger
 parseNegativeInteger :: Parser JValue
 parseNegativeInteger = fmap (\x -> N (fromIntegral (-x))) $ (parseNegativeSign *> parseInteger)
 
---parseUnsignedDecimal:: Parser JValue
---parseUnsignedDecimal = (\x y -> N (read x)) <$> ( (oneOrMore (satisfy isNumber)) <* parseDecimalPoint *> (zeroOrMore (satisfy isNumber)) )
+-- examples of decimal points
+-- 0.
+-- .5
+-- 1.0
+-- -2.0
 
---parseSignedDecimal :: Parser JValue
---parseSignedDecimal = (\x y -> N (read (-x)))) <$> (parseNegativeSign *> (oneOrMore (satisfy isNumber)) <* parseDecimalPoint *> (zeroOrMore (satisfy isNumber)) )
+-- 1.
+parsePositiveDecimal:: Parser JValue
+parsePositiveDecimal = (\x _ y -> f x y) <$> ( (oneOrMore (satisfy isNumber)) <*> parseDecimalPoint <*> (zeroOrMore (satisfy isNumber)) )
+  where 
+  	f x [] = N (read x)
+  	f x y  = N (-(readWholeAndDecimal (read x) (read y)))
+
+--parseNegativeDecimal :: Parser JValue
+--parseNegativeDecimal = f <$> (parseNegativeSign *> (oneOrMore (satisfy isNumber)) <* parseDecimalPoint *> (zeroOrMore (satisfy isNumber)) )
+--  where 
+--  	f x [] = N (read (-x))
+--  	f x y  = N (-(readWholeAndDecimal x y))
+
+--parsePositiveDecimalOnly :: Parser JValue
+--parsePositiveDecimalOnly = N <$> (parseDecimalPoint *> parseInteger)
 
 parseNegativeSign :: Parser Char
 parseNegativeSign = satisfy (== '-')
