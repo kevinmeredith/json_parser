4fdeb918e919420757f8d78ec92ea5265c90032b
Kevin Meredith <kevin.m.meredith@gmail.com>
2015-02-12 22:28:26 -0500

    Added the currently unneeded 'endOfString,' and then got SO help for the 'parseNumberJValue.'

--- JsonParser.hs
+++ JsonParser.hs
@@ -10,8 +10,8 @@ import Data.Char
 parseStringJValue :: Parser JValue
 parseStringJValue = (\x -> S x) <$> zeroOrMore (satisfy (const True))
 
---parseNumberJValue :: Parser JValue
---parseNumberJValue = alt parseUnsignedNumber
+parseNumberJValue :: Parser JValue
+parseNumberJValue = alt (alt parsePositiveDecimal parseNegativeDecimal) (alt parsePositiveInteger parseNegativeInteger)
 
 parsePositiveInteger :: Parser JValue
 parsePositiveInteger = fmap (\x -> N (fromIntegral x)) parseInteger 
@@ -25,21 +25,17 @@ parseNegativeInteger = fmap (\x -> N (fromIntegral (-x))) $ (parseNegativeSign *
 -- 1.0
 -- -2.0
 
--- 1.
 parsePositiveDecimal:: Parser JValue
-parsePositiveDecimal = (\x _ y -> f x y) <$> ( (oneOrMore (satisfy isNumber)) <*> parseDecimalPoint <*> (zeroOrMore (satisfy isNumber)) )
-  where 
-  	f x [] = N (read x)
-  	f x y  = N (-(readWholeAndDecimal (read x) (read y)))
+parsePositiveDecimal = (\x _ y -> f x y) <$> (oneOrMore (satisfy isNumber)) <*> parseDecimalPoint <*> (zeroOrMore (satisfy isNumber))
+  where
+    f x [] = N (read x)
+    f x y  = N ((readWholeAndDecimal (read x) (read y)))
 
---parseNegativeDecimal :: Parser JValue
---parseNegativeDecimal = f <$> (parseNegativeSign *> (oneOrMore (satisfy isNumber)) <* parseDecimalPoint *> (zeroOrMore (satisfy isNumber)) )
---  where 
---  	f x [] = N (read (-x))
---  	f x y  = N (-(readWholeAndDecimal x y))
-
---parsePositiveDecimalOnly :: Parser JValue
---parsePositiveDecimalOnly = N <$> (parseDecimalPoint *> parseInteger)
+parseNegativeDecimal:: Parser JValue
+parseNegativeDecimal = (\_ x _ y -> f x y) <$> parseNegativeSign <*> (oneOrMore (satisfy isNumber)) <*> parseDecimalPoint <*> (zeroOrMore (satisfy isNumber))
+  where
+    f x [] = N (read x)
+    f x y  = N (-(readWholeAndDecimal (read x) (read y)))
 
 parseNegativeSign :: Parser Char
 parseNegativeSign = satisfy (== '-')
@@ -56,5 +52,14 @@ type Decimal = Integer
 
 readWholeAndDecimal :: Whole -> Decimal -> Double
 readWholeAndDecimal w d = read $ (show w) ++ "." ++ (show d)
+
+data EndOfString = EndOfString deriving Show
+
+-- originally I thought it was needed for -1234.3.3, but perhaps the remaining input can be parsed afterwards
+endOfString :: Parser EndOfString
+endOfString = Parser f
+  where
+    f [] = Just (EndOfString, [])
+    f _  = Nothing
 
 
