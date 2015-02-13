e0e41cab952733f2a666faa7c2644d25e1dfdc82
Kevin Meredith <kevin.m.meredith@gmail.com>
2015-02-12 23:13:44 -0500

    Implemented parseBool, but then ran into issues with parseArray and parseString again.

--- JsonParser.hs
+++ JsonParser.hs
@@ -7,8 +7,17 @@ import AParser
 import Control.Applicative
 import Data.Char
 
+parseJValue :: Parser JValue
+parseJValue = alt (alt parseBooleanJValue parseNumberJValue) parseStringJValue 
+
+parseArrayJValue :: Parser Json
+parseArrayJValue = fmap Arr (zeroOrMore $ spaces *> parseJValue <* spaces <* (satisfy (== ',')))
+
+parseBooleanJValue :: Parser JValue
+parseBooleanJValue = fmap B parseBool
+
 parseStringJValue :: Parser JValue
-parseStringJValue = (\x -> S x) <$> zeroOrMore (satisfy (const True))
+parseStringJValue = S <$> (spaces *> (char '"') *> (zeroOrMore (satisfy (const True))) <* (char '"'))
 
 parseNumberJValue :: Parser JValue
 parseNumberJValue = alt (alt parsePositiveDecimal parseNegativeDecimal) (alt parsePositiveInteger parseNegativeInteger)
@@ -19,6 +28,8 @@ parsePositiveInteger = fmap (\x -> N (fromIntegral x)) parseInteger
 parseNegativeInteger :: Parser JValue
 parseNegativeInteger = fmap (\x -> N (fromIntegral (-x))) $ (parseNegativeSign *> parseInteger)
 
+-- TODO: implement `not`
+
 -- examples of decimal points
 -- 0.
 -- .5
@@ -26,7 +37,7 @@ parseNegativeInteger = fmap (\x -> N (fromIntegral (-x))) $ (parseNegativeSign *
 -- -2.0
 
 parsePositiveDecimal:: Parser JValue
-parsePositiveDecimal = (\x _ y -> f x y) <$> (oneOrMore (satisfy isNumber)) <*> parseDecimalPoint <*> (zeroOrMore (satisfy isNumber))
+parsePositiveDecimal = (\x _ y -> f x y) <$> (oneOrMore (satisfy isNumber)) <*> parseDecimalPoint <*> (zeroOrMore (satisfy isNumber)) 
   where
     f x [] = N (read x)
     f x y  = N ((readWholeAndDecimal (read x) (read y)))
@@ -54,6 +65,13 @@ readWholeAndDecimal :: Whole -> Decimal -> Double
 readWholeAndDecimal w d = read $ (show w) ++ "." ++ (show d)
 
 data EndOfString = EndOfString deriving Show
+
+parseBool :: Parser Bool
+parseBool = Parser f
+  where
+    f ('t':'r':'u':'e':xs) 	   = Just (True, xs)
+    f ('f':'a':'l':'s':'e':xs) = Just (False, xs)
+    f _                        = Nothing
 
 -- originally I thought it was needed for -1234.3.3, but perhaps the remaining input can be parsed afterwards
 endOfString :: Parser EndOfString
